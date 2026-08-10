c1f04786ac91100e77049650eefb25e1ad085547
shubhambej27 <shubhambej2704@gmail.com>
2026-08-10 17:35:32 +0530

    shubham

---
+++ Solution.java
@@ -0,0 +1,15 @@
+```java
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+
+        return a;
+    }
+}
+```
